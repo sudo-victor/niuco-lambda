38b503c59e67d0c19058e7d983d9be4d97a9a74a
Victor de Souto <victor.souto@hotmail.com>
2024-06-23 12:30:48 -0300

    feat: list users function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import 'dotenv/config'
 import { LambdaFunctionURLEvent } from "aws-lambda";
+import { MakeListUser } from './factory';
 
 export const handler = async (event: LambdaFunctionURLEvent) => {
+  const usecase = MakeListUser.make()
+  const result = await usecase.execute()
+  if (result.isLeft()) {
+    return {
+      statusCode: 500,
+      body: result.value
+    };
+  }
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(result.value, null, 2),
   };
 };
